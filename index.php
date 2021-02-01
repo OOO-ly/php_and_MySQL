ce033fa7a862b358bcd769a762df8c808fb23069
OOO-ly <57537902+OOO-ly@users.noreply.github.com>
2021-02-01 11:27:40 +0900

    tnj_day6_php&mysql_1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>HTML</h1>
+    <ol>
+        <li>HTML</li>
+    </ol>
+    <h2>Welcome</h2>
+    Lorem ipsum dolor sit amet consectetur
+</body>
+
+</html>
+
