47c8e95bed27a0569544c8c40bf552192d1f9d2f
OOO-ly <57537902+OOO-ly@users.noreply.github.com>
2021-02-10 11:22:20 +0900

    tnj_day12_nav_bar_complete

--- new_index.php
+++ new_index.php
@@ -11,88 +11,35 @@ $title = "tnj_tutorial"
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=\, initial-scale=1.0">
     <title><?= $title ?></title>
-    <!-- <link rel="stylesheet" href="../style/login.css"> -->
     <link rel="stylesheet" href="./style/nav.css">
-    <!-- <link rel="stylesheet" href="/style/flex_index.css"> -->
 </head>
 
 <body>
-<<<<<<< HEAD
-    
-    <nav>
-        <ul class="nav-container">
-            <li class="index_asd"><a href="">home</a></li>
-            <li class="nav_item index_"><a href="./home.php">home</a></li>
-            <li class="nav_item"><a href="./produce.php">회사소개</a></li>
-            <li class="nav_item"><a href="./comunity.php">커뮤니티</a></li>
-            <li class="nav_item"><a href="./contactus.php">고객지원</a></li>
-=======
+
+   
     <nav>
         <ul class="nav-container">
             <a class="nav-logo" href="./new_index.php">
-                <img src="./media/profile.png" alt="o3" viewBox="0 0 100 100" width="30" height="30">
+                <img src="./media/profile.png" alt="nav_logo" >
             </a>
-            <!-- <li class="nav_item"><a href="./home.php">home</a></li> -->
-            <li class="nav_item"><a href="./produce.php">회사소개</a></li>
-            <li class="nav_item"><a href="./comunity.php">커뮤니티</a></li>
-            <li class="nav_item"><a href="./contactus.php">고객지원</a></li>
-       
->>>>>>> 242c1dbf78d4d7e63783fb6dbad9e1f5157dec54
+            <li class="nav_item"><a href="./produce.php" class="produce_">회사소개</a></li>        
+            <li class="nav_item"><a href="./comunity.php" class="coumunity_">커뮤니티</a></li>
+            <li class="nav_item"><a href="./contactus.php" class="contact_">고객지원</a></li>
+            <div class="login_box">
+                <li class="nav_item"><a href="" class="sign_">로그인</a></li>
+                  
+                <li class="nav_item"><a href="" class="sign_up">회원가입</a></li>
+            </div>
         </ul>
     </nav>
 
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
+    <div class="content-container">
+ 
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, amet nisi enim nemo iste doloribus fugiat, asperiores laudantium veniam recusandae, illo maiores sequi at? Quis harum autem quo tenetur in.</p>
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur adipisci molestias enim expedita, cum odio fuga, tempore ad quod fugit mollitia magni iste modi. Laudantium, quo tempore. Molestias, perspiciatis quis.</p>
-
-
+    </div>
 </body>
 
 </html>
